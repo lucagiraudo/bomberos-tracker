a63c1cb9180991b57fd7b803b3fe3eed460c546d
lucagiraudo <lucagiraudo98@gmail.com>
2022-07-27 10:47:31 +0200

    V2 completed

--- code.js
+++ code.js
@@ -11,102 +11,173 @@ let selected_component;
 let code_textnode;
 var color_code;
 var project_type;
+// team or person switch
+var calc_mode = "team";
 // ------- SUMMARY ------- //
 var summary_array;
 var summary_project_code;
 //to calculate total days for the project
-var total_days;
+var booked_days;
 // ------- ACTIVITY ------- //
-var activity_totalhour_counter = 0;
-var activity_totalday_counter = 0;
+var totalhour_counter = 0;
+var used_day_counter = 0;
 var activity_array;
 var activity_project_code;
 var activity_skipped = 0;
 var activity_project_code_text;
+var activity_person;
 // Figma file to refer to for components:
 // https://www.figma.com/community/file/969639692881611105/Planner---Bomberos
 // ------- FUNCTIONS ------- //
-// -- MAIN -- //
-function calculate() {
-    //reset all
-    total_days = 0;
-    activity_totalhour_counter = 0;
-    activity_skipped = 0;
-    //calculate starts
-    if (figma.currentPage.selection.length !== 1) {
-        figma.closePlugin("select a 'project-summary' component");
-        return;
-    }
-    selected_component = figma.currentPage.selection[0];
-    project_type = selected_component.componentProperties["progetto"].value;
-    code_textnode = selected_component.findOne(n => n.name === "summary_project_code");
-    if (!code_textnode) {
-        //check if user has selected an "activity" component
-        code_textnode = selected_component.findOne(n => n.name === "activity_project_code");
-        console.log("activity: " + code_textnode.characters.toUpperCase());
-        if (!code_textnode) {
-            figma.closePlugin("select a 'project-summary' or 'activity' component");
-            return;
+figma.on('selectionchange', () => {
+    console.log("name:" + selected_component.name);
+    if (figma.currentPage.selection[0]) {
+        if (updateSelectedElementInfos()) {
+            console.log("loader attivo pre");
+            figma.ui.postMessage({
+                loaderStatus: {
+                    status: 1,
+                }
+            });
+            calculate();
         }
     }
-    color_code = selected_component.findOne(n => n.name === "project_color_code").fills[0].color;
-    console.log("color_code: " + Math.round(color_code.g * 255));
-    if (code_textnode.type !== 'TEXT') {
-        figma.closePlugin("nodo non riconosciuto (in realtà da fare bene questa gestione di errori)");
-        return;
+    else {
+        figma.notify('Seleziona un blocco "Activity" o "Project-Summary"', { timeout: 900 });
     }
+});
+// ------- PROGRAM ------- //
+figma.showUI(__html__, { width: 280, height: 400 });
+figma.ui.onmessage = (message) => {
+    console.log(message);
+    if (message == "calculate") {
+        calculate();
+    }
+    else if (message == "team" || message == "person") {
+        calc_mode = message;
+        calculate();
+    }
+};
+selected_component = figma.currentPage.selection[0];
+if (selected_component.name === "activity-v2" || selected_component.name === "project-summary-v2") {
+    updateSelectedElementInfos();
+    calculate();
+}
+figma.closePlugin;
+function updateSelectedElementInfos() {
+    selected_component = figma.currentPage.selection[0];
+    console.log("ACTIVITY SELECTED");
+    if (selected_component.name === "activity-v2" || selected_component.name === "project-summary-v2") {
+        //collect infos about the project
+        project_type = selected_component.componentProperties["project"].value;
+        code_textnode = selected_component.findOne(n => n.name === "project_code");
+        color_code = selected_component.findOne(n => n.name === "project_color_code").fills[0].color;
+        console.log("selected project type: " + project_type);
+        //initials only with activity component
+        if (selected_component.name === "activity-v2") {
+            activity_person = selected_component.findOne(n => n.name === "initials").characters;
+            //send to UI
+            figma.ui.postMessage({
+                projectInfos: {
+                    from: "activity",
+                    project_name: code_textnode.characters.toUpperCase(),
+                    project_type: project_type,
+                    project_color_red: Math.round(color_code.r * 255),
+                    project_color_green: Math.round(color_code.g * 255),
+                    project_color_blue: Math.round(color_code.b * 255),
+                    activity_person: activity_person
+                }
+            });
+            //booked days only with summary component
+        }
+        else if (selected_component.name === "project-summary") {
+            booked_days = selected_component.findOne(n => n.name === "summary_booked_days");
+            //send to UI
+            figma.ui.postMessage({
+                projectInfos: {
+                    from: "summary",
+                    project_name: code_textnode.characters,
+                    project_type: project_type,
+                    project_color_red: Math.round(color_code.r * 255),
+                    project_color_green: Math.round(color_code.g * 255),
+                    project_color_blue: Math.round(color_code.b * 255),
+                    booked_days: booked_days
+                }
+            });
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function calculate() {
+    //reset all
+    totalhour_counter = 0;
+    used_day_counter = 0;
+    activity_skipped = 0;
+    booked_days = 0;
     //search for all activity with selected project code
-    activity_array = figma.currentPage.findAll(n => n.name === "activity_v2");
+    activity_array = figma.currentPage.findAll(n => n.name === "activity-v2");
     console.log("activity items number: " + activity_array.length);
     activity_array.forEach((activity) => {
-        activity_project_code = activity.findOne(n => n.name === "activity_project_code");
+        // console.log("current activity: " + activity);
+        activity_project_code = activity.findOne(n => n.name === "project_code");
         if (activity_project_code) {
             var activity_project_code_text = activity_project_code.characters.toUpperCase();
             // //calculate total days
             if (activity_project_code_text === code_textnode.characters.toUpperCase()) {
-                activity_totalhour_counter += +activity.componentProperties["ore"].value;
+                if (calc_mode == "team") {
+                    totalhour_counter += +activity.componentProperties["hours"].value;
+                }
+                else {
+                    // only if same initials
+                    console.log("person mode");
+                    if (activity.findOne(n => n.name === "initials").characters === activity_person) {
+                        totalhour_counter += +activity.componentProperties["hours"].value;
+                    }
+                }
             }
         }
     });
-    activity_totalday_counter = activity_totalhour_counter / 8;
-    console.log("hours: " + activity_totalhour_counter);
-    total_days = selected_component.findOne(n => n.name === "summary_total_days");
-    if (total_days) {
-        //summary_case
-        total_days = total_days.characters;
-    }
-    else {
-        //activity case, we need to find the summary
-        summary_array = figma.currentPage.findAll(n => n.name === "project-summary");
-        summary_array.forEach((summary) => {
-            summary_project_code = summary.findOne(n => n.name === "summary_project_code").characters;
-            if (summary_project_code === code_textnode.characters.toUpperCase()) {
-                total_days = summary.findOne(n => n.name === "summary_total_days").characters;
+    used_day_counter = totalhour_counter / 8;
+    console.log("hours: " + totalhour_counter);
+    console.log("days: " + used_day_counter);
+    //booked days only if Package project type
+    if (project_type === "Package") {
+        console.log("sending Package");
+        if (booked_days) {
+            //summary_case
+            booked_days = booked_days.characters;
+        }
+        else {
+            //activity case, we need to find the summary
+            summary_array = figma.currentPage.findAll(n => n.name === "project-summary-v2");
+            summary_array.forEach((summary) => {
+                summary_project_code = summary.findOne(n => n.name === "project_code").characters.toUpperCase;
+                if (summary_project_code === code_textnode.characters.toUpperCase()) {
+                    console.log("i'm in");
+                    booked_days = summary.findOne(n => n.name === "summary_booked_days").characters;
+                }
+            });
+        }
+        //send to UI
+        figma.ui.postMessage({
+            daysCount: {
+                from: "Package",
+                booked_days: booked_days,
+                used_day_counter: used_day_counter,
             }
         });
     }
-    var days_todo = total_days - activity_totalday_counter;
-    //send to UI
-    figma.ui.postMessage({
-        projectMessage: {
-            project_name: code_textnode.characters,
-            booked_days: total_days,
-            proj_number: activity_totalday_counter,
-            project_type: project_type,
-            activity_skipped: activity_skipped,
-            project_color_red: Math.round(color_code.r * 255),
-            project_color_green: Math.round(color_code.g * 255),
-            project_color_blue: Math.round(color_code.b * 255)
-        }
-    });
-}
-function calculate_by_summary() {
-}
-// ------- PROGRAM ------- //
-figma.showUI(__html__, { width: 280, height: 400 });
-figma.ui.onmessage = (message) => {
-    if (message == "calculate") {
-        calculate();
+    else if ((project_type === "Monthly support")) {
+        console.log("sending Monthly support");
+        //send to UI
+        figma.ui.postMessage({
+            daysCount: {
+                from: "Monthly support",
+                used_day_counter: used_day_counter,
+            }
+        });
     }
-};
-figma.closePlugin;
+}
